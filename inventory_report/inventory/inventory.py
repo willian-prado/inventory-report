b9aeb49413420e15101542bcdcbf92e4148a069c
Willian Prado <willian_prado@hotmail.com>
2022-02-24 20:12:58 -0300

    Requisito 5

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import xml.etree.ElementTree as ET
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
@@ -12,12 +13,28 @@ class Inventory:
                     file, delimiter=",", quotechar='"'
                 )
                 data = list(products_reader)
+
         elif path.endswith(".json"):
             with open(path) as file:
                 data = json.load(file)
+
+        elif path.endswith(".xml"):
+            tree = ET.parse(path)
+            root = tree.getroot()
+            data = []
+
+            for record in root.iter('record'):
+                product = {}
+                for entries in record:
+                    product[entries.tag] = entries.text
+                data.append(product)
 
         return (
             SimpleReport.generate(data)
             if (type == "simples")
             else CompleteReport.generate(data)
         )
+
+
+path = "inventory_report/data/inventory.xml"
+my_report = Inventory.import_data(path, "simples")
